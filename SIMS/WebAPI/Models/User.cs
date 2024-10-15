c7d81c44cbd8983ce548b03f2f334f0724ff0b2f
xandau <is231024@fhstp.ac.at>
2024-10-15 12:08:56 +0200

    sql user sec trial

--- SIMS/WebAPI/Models/User.cs
+++ SIMS/WebAPI/Models/User.cs
@@ -1,55 +1,86 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using WebAPI.Enums;
+using WebAPI.Models;
 
-namespace WebAPI.Models
+[Table("USERS")]
+public class User
 {
-    [Table("USERS")]
-    public class User
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("UserID")]
+    public long UserID { get; set; }
+
+    [Required]
+    [Column("User_UUID", TypeName = "VARCHAR(300)")]
+    public Guid UserUUID { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    [Column("USERNAME")]
+    public required string UserName { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    [Column("FIRSTNAME")]
+    public required string FirstName { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    [Column("LASTNAME")]
+    public required string LastName { get; set; }
+
+    [Required]
+    [Column("PASSWORD_HASH")]
+    public required byte[] PasswordHash { get; set; }
+
+    [Required]
+    [Column("PASSWORD_SALT")]
+    public required byte[] PasswordSalt { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    [Column("EMAIL")]
+    public required string Email { get; set; }
+
+    [Required]
+    [Column("ROLE")]
+    public required ERoles Role { get; set; }
+
+    [Required]
+    [Column("BLOCKED")]
+    public required bool Blocked { get; set; }
+
+    public List<Ticket>? Tickets { get; set; }
+
+    //method for hashing passwords
+    public void SetPassword(string password)
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Column("UserID")]
-        public long UserID { get; set; }
+        PasswordSalt = new byte[16];
+        RandomNumberGenerator.Fill(PasswordSalt); // Generate a random salt
 
-        [Required]
-        [Column("User_UUID", TypeName = "VARCHAR(300)")]
-        public Guid UserUUID { get; set; }
+        // Hash the password with salt
+        this.PasswordHash = HashPassword(password, PasswordSalt);
+    }
 
-        [Required]
-        [StringLength(50)]
-        [Column("USERNAME")]
-        public required string UserName { get; set; }
+    // Verify password
+    public bool VerifyPassword(string password)
+    {
+        var hash = HashPassword(password, PasswordSalt);
+        return PasswordHash.SequenceEqual(hash);
+    }
 
-        [Required]
-        [StringLength(50)]
-        [Column("FIRSTNAME")]
-        public required string FirstName { get; set; }
-
-        [Required]
-        [StringLength(50)]
-        [Column("LASTNAME")]
-        public required string LastName { get; set; }
-
-        [Required]
-        [StringLength(300)]
-        [Column("PASSWORD")]
-        public required string Password { get; set; }
-
-        [Required]
-        [StringLength(50)]
-        [Column("EMAIL")]
-        public required string Email { get; set; }
-
-        [Required]
-        [Column("ROLE")]
-        public required ERoles Role { get; set; }
-
-        [Required]
-        [Column("BLOCKED")]
-        public required bool Blocked { get; set; }
-
-        public List<Ticket>? Tickets { get; set; }
-
+    // Hash password method using PBKDF2
+    private byte[] HashPassword(string password, byte[] salt)
+    {
+        return KeyDerivation.Pbkdf2(
+            password: password,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 10000,
+            numBytesRequested: 32
+        );
     }
 }
