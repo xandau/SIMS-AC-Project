4bf75c1c3567e79e75fb98791335264ec1de43e4
xandau <is231024@fhstp.ac.at>
2024-10-16 17:32:54 +0200

    register working
    yippe

--- SIMS/AngularClient/hello-world/src/app/app.routes.ts
+++ SIMS/AngularClient/hello-world/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
 ];
