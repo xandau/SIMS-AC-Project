755bafa32aea98ffec0b3c79230f6b1caf78aa77
xandau <is231024@fhstp.ac.at>
2024-10-16 20:03:41 +0200

    help?

---
+++ SIMS/AngularClient/hello-world/.angular/cache/18.2.8/hello-world/vite/deps_ssr/@angular_material_form-field.js
@@ -0,0 +1,45 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  MAT_ERROR,
+  MAT_FORM_FIELD,
+  MAT_FORM_FIELD_DEFAULT_OPTIONS,
+  MAT_PREFIX,
+  MAT_SUFFIX,
+  MatError,
+  MatFormField,
+  MatFormFieldControl,
+  MatFormFieldModule,
+  MatHint,
+  MatLabel,
+  MatPrefix,
+  MatSuffix,
+  getMatFormFieldDuplicatedHintError,
+  getMatFormFieldMissingControlError,
+  getMatFormFieldPlaceholderConflictError,
+  matFormFieldAnimations
+} from "./chunk-WWV52U6L.js";
+import "./chunk-LABZZ4ZM.js";
+import "./chunk-WHBO2SNH.js";
+import "./chunk-6K3IK2DV.js";
+import "./chunk-TW4XVQZD.js";
+import "./chunk-NQ4HTGF6.js";
+export {
+  MAT_ERROR,
+  MAT_FORM_FIELD,
+  MAT_FORM_FIELD_DEFAULT_OPTIONS,
+  MAT_PREFIX,
+  MAT_SUFFIX,
+  MatError,
+  MatFormField,
+  MatFormFieldControl,
+  MatFormFieldModule,
+  MatHint,
+  MatLabel,
+  MatPrefix,
+  MatSuffix,
+  getMatFormFieldDuplicatedHintError,
+  getMatFormFieldMissingControlError,
+  getMatFormFieldPlaceholderConflictError,
+  matFormFieldAnimations
+};
+//# sourceMappingURL=@angular_material_form-field.js.map
