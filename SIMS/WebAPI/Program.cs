190d2a05ea637749f8235dd9063490fc0bec13e9
imprint6 <fabian.treber03@gmail.com>
2024-10-16 19:50:12 +0200

    Programm.cs option for Json cycle

--- SIMS/WebAPI/Program.cs
+++ SIMS/WebAPI/Program.cs
@@ -22,7 +22,7 @@ namespace WebAPI
 
             builder.Services.AddControllers().AddJsonOptions(options =>
             {
-                //options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+                options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
             });
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
