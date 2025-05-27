134d2df445c27d01f3d64f0fb28cf6d6f956a7ef
Xandau <admin@borpi.net>
2025-05-28 00:29:31 +0200

    i cant ughh

--- SIMS/AngularClient/hello-world/src/app/app.config.ts
+++ SIMS/AngularClient/hello-world/src/app/app.config.ts
@@ -1,17 +1,9 @@
-import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {
-  HTTP_INTERCEPTORS,
-  provideHttpClient,
-  withInterceptorsFromDi
-} from '@angular/common/http';
-
-import { CookieModule } from 'ngx-cookie-service';
-import { AuthInterceptor } from './core/auth/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
