6a3dab412c7f5479d152b70a9dd6d6c4e69e4d96
Xandau <admin@borpi.net>
2025-05-27 16:44:37 +0200

    help me

--- SIMS/AngularClient/hello-world/src/assets/config.js
+++ SIMS/AngularClient/hello-world/src/assets/config.js
@@ -1,4 +1,5 @@
 (function(window) {
     window.__env = window.__env || {};
     window.__env.WEB_API_URL = "http://test-lb-1991875074.eu-central-1.elb.amazonaws.com";
+    window.__env.WEB_OWN_DOMAIN = "http://fronteend-lb-1789506076.eu-central-1.elb.amazonaws.com";
   })(this);
