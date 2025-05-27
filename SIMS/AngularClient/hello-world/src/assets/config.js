c36bdd3264a8678e125da74cbef892896bfe43b4
Xandau <admin@borpi.net>
2025-05-27 13:21:08 +0200

    http

--- SIMS/AngularClient/hello-world/src/assets/config.js
+++ SIMS/AngularClient/hello-world/src/assets/config.js
@@ -1,4 +1,4 @@
 (function(window) {
     window.__env = window.__env || {};
-    window.__env.WEB_API_URL = "test-lb-1991875074.eu-central-1.elb.amazonaws.com";
+    window.__env.WEB_API_URL = "http://test-lb-1991875074.eu-central-1.elb.amazonaws.com";
   })(this);
